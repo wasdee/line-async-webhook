5b495968955e263061f6896350ebbe5e61f951b5
Nutchanon Ninyawee <nutchanon@codustry.com>
2023-06-06 08:49:56 +0700

    🌟ADD: add patch function

--- line_async_webhook/__init__.py
+++ line_async_webhook/__init__.py
@@ -78,3 +78,68 @@ class AsyncWebhookHandler(WebhookHandler):
     @staticmethod
     def __get_handler_key(event, message=None):
         return WebhookHandler.__get_handler_key(event, message)
+
+
+async def handle_async(self, body, signature):
+    """Handle webhook.
+
+    :param str body: Webhook request body (as text)
+    :param str signature: X-Line-Signature value (as text)
+    """
+    payload = self.parser.parse(body, signature, as_payload=True)
+
+    async with asyncer.create_task_group() as task_group:
+        for event in payload.events:
+            func = None
+            key = None
+
+            if isinstance(event, MessageEvent):
+                key = self._WebhookHandler__get_handler_key(
+                    event.__class__, event.message.__class__)
+                func = self._handlers.get(key, None)
+
+            if func is None:
+                key = self._WebhookHandler__get_handler_key(event.__class__)
+                func = self._handlers.get(key, None)
+
+            if func is None:
+                func = self._default
+
+            if func is None:
+                logger.info('No handler of ' + key + ' and no default handler')
+            else:
+                if inspect.iscoroutinefunction(func):
+                    task_group.soonify(self.__invoke_func_async)(func, event, payload)
+                else:
+                    self.__invoke_func(func, event, payload)
+
+
+
+def add_handler(self, func, event, message=None):
+    if isinstance(message, (list, tuple)):
+        for it in message:
+            self._WebhookHandler__add_handler(func, event, message=it)
+    else:
+        self._WebhookHandler__add_handler(func, event, message=message)
+
+
+    
+async def invoke_func_async(cls, func, event, payload):
+    ## START: copy from WebhookHandler.__invoke_func, but added function_return capture
+    (has_varargs, args_count) = cls._WebhookHandler__get_args_count(func)
+    if has_varargs or args_count == 2:
+        function_return = func(event, payload.destination)
+    elif args_count == 1:
+        function_return = func(event)
+    else:
+        function_return = func()
+    ## END: copy from WebhookHandler.__invoke_func, but added function_return capture
+
+    await function_return
+
+
+
+def patch_line_sdk_python():
+    setattr(WebhookHandler, "handle", handle_async)
+    setattr(WebhookHandler, "add_handler", add_handler)
+    setattr(WebhookHandler, "__invoke_func_async", classmethod(invoke_func_async))
